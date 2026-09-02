c1ef6520d355ebc2ea9c38ac2218769e98b68c06
henrique.matheus <henriquematheussilva.21@gmail.com>
2026-09-02 01:20:13 -0300

    feat: configuracao das matrizes clinicas farnsworth e lanthony

---
+++ lpnec/js/colors.js
@@ -0,0 +1,41 @@
+const COLOR_PALETTES = {
+    // Farnsworth D-15 (Saturado)
+    farnsworth: [
+        { id: 0,  hex: "rgb(55, 129, 193)", fixed: true },
+        { id: 1,  hex: "rgb(54, 131, 185)", fixed: false },
+        { id: 2,  hex: "rgb(53, 131, 180)", fixed: false },
+        { id: 3,  hex: "rgb(59, 132, 167)", fixed: false },
+        { id: 4,  hex: "rgb(57, 133, 156)", fixed: false },
+        { id: 5,  hex: "rgb(59, 134, 144)", fixed: false },
+        { id: 6,  hex: "rgb(63, 135, 130)", fixed: false },
+        { id: 7,  hex: "rgb(88, 132, 115)", fixed: false },
+        { id: 8,  hex: "rgb(108, 129, 100)", fixed: false },
+        { id: 9,  hex: "rgb(131, 123, 93)", fixed: false },
+        { id: 10, hex: "rgb(144, 118, 96)", fixed: false },
+        { id: 11, hex: "rgb(159, 109, 124)", fixed: false },
+        { id: 12, hex: "rgb(156, 109, 137)", fixed: false },
+        { id: 13, hex: "rgb(146, 112, 153)", fixed: false },
+        { id: 14, hex: "rgb(143, 111, 164)", fixed: false },
+        { id: 15, hex: "rgb(128, 115, 178)", fixed: false }
+    ],
+
+// Lanthony D-15 (Dessaturado) 
+    lanthony: [
+        { id: 0,  hex: "rgb(161, 203, 243)", fixed: true },
+        { id: 1,  hex: "rgb(161, 205, 238)", fixed: false },
+        { id: 2,  hex: "rgb(161, 205, 229)", fixed: false },
+        { id: 3,  hex: "rgb(162, 206, 219)", fixed: false },
+        { id: 4,  hex: "rgb(162, 207, 210)", fixed: false },
+        { id: 5,  hex: "rgb(164, 207, 200)", fixed: false },
+        { id: 6,  hex: "rgb(165, 208, 191)", fixed: false },
+        { id: 7,  hex: "rgb(177, 206, 176)", fixed: false },
+        { id: 8,  hex: "rgb(195, 203, 164)", fixed: false },
+        { id: 9,  hex: "rgb(216, 197, 157)", fixed: false },
+        { id: 10, hex: "rgb(227, 192, 162)", fixed: false },
+        { id: 11, hex: "rgb(239, 186, 187)", fixed: false },
+        { id: 12, hex: "rgb(236, 186, 195)", fixed: false },
+        { id: 13, hex: "rgb(231, 186, 206)", fixed: false },
+        { id: 14, hex: "rgb(229, 186, 216)", fixed: false },
+        { id: 15, hex: "rgb(214, 190, 224)", fixed: false }
+    ]
+};
